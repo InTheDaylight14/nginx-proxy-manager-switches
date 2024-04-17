17cfb133d40a8b163065e5f79e1aece1954bee74
Emory Penney <emory.j.penney@intel.com>
2024-04-17 15:12:46 -0700

    Update __init__.py

--- custom_components/npm_switches/__init__.py
+++ custom_components/npm_switches/__init__.py
@@ -59,8 +59,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     for platform in PLATFORMS:
         if entry.options.get(platform, True):
             coordinator.platforms.append(platform)
-            hass.async_add_job(
-                hass.config_entries.async_forward_entry_setup(entry, platform)
+            entry.async_create_task(hass(
+                hass, hass.config_entries.async_forward_entry_setup(entry, platform)
             )
 
     entry.async_on_unload(entry.add_update_listener(async_reload_entry))
