bf765802dd54ab8937472c7b25a80c2d80020274
Penney, Emory J <treadstoneit@gmail.com>
2024-04-26 15:26:08 +0000

    Completely different approach

--- custom_components/npm_switches/__init__.py
+++ custom_components/npm_switches/__init__.py
@@ -59,9 +59,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     for platform in PLATFORMS:
         if entry.options.get(platform, True):
             coordinator.platforms.append(platform)
-            entry.async_create_task(
-                hass, hass.config_entries.async_forward_entry_setup(entry, platform)
-            )
+            await hass.config_entries.async_forward_entry_setup(entry, platform)
 
     entry.async_on_unload(entry.add_update_listener(async_reload_entry))
     return True
