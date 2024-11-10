91762e00114ad498afbca453c66cf4fb39ad9305
Kyle <millerkyle72@gmail.com>
2024-11-10 18:58:00 +0000

    add redir sensors

--- custom_components/npm_switches/sensor.py
+++ custom_components/npm_switches/sensor.py
@@ -11,14 +11,16 @@ async def async_setup_entry(hass, entry, async_add_entities):
     """Setup sensor platform."""
     coordinator = hass.data[DOMAIN][entry.entry_id]
     entities = []
-    entities.append(NpmSwitchesSensor(coordinator, entry, "enabled"))
-    entities.append(NpmSwitchesSensor(coordinator, entry, "disabled"))
+    entities.append(NpmSwitchesProxySensor(coordinator, entry, "enabled"))
+    entities.append(NpmSwitchesProxySensor(coordinator, entry, "disabled"))
+    entities.append(NpmSwitchesRedirSensor(coordinator, entry, "enabled"))
+    entities.append(NpmSwitchesRedirSensor(coordinator, entry, "disabled"))
 
     async_add_entities(entities, True)
 
 
-class NpmSwitchesSensor(NpmSwitchesEntity, SensorEntity):
-    """integration_blueprint Sensor class."""
+class NpmSwitchesProxySensor(NpmSwitchesEntity, SensorEntity):
+    """NPM Switches Proxy Sensor class."""
 
     def __init__(
         self,
@@ -42,8 +44,41 @@ class NpmSwitchesSensor(NpmSwitchesEntity, SensorEntity):
     def native_value(self):
         """Return the native value of the sensor."""
         if self.sensor_name == "enabled":
-            return self.coordinator.api.num_enabled
-        return self.coordinator.api.num_disabled
+            return self.coordinator.api.num_proxy_enabled
+        return self.coordinator.api.num_proxy_disabled
+
+    @property
+    def icon(self):
+        """Return the icon of the sensor."""
+        return self._attr_icon
+
+class NpmSwitchesRedirSensor(NpmSwitchesEntity, SensorEntity):
+    """NPM Switches Redir Sensor class."""
+
+    def __init__(
+        self,
+        coordinator: NpmSwitchesUpdateCoordinator,
+        entry: ConfigEntry,
+        name: str,
+    ) -> None:
+        """Initialize proxy switch entity."""
+        super().__init__(coordinator, entry)
+        self.proxy_id = name  # Unique ID relies on self.proxy_id
+        self.sensor_name = self.proxy_id
+        self._attr_icon = "mdi:steering"
+        self.friendly_name = "NPM " + self.sensor_name.capitalize() + " Redirection Hosts"
+
+    # @property
+    # def name(self):
+    #     """Return the name of the sensor."""
+    #     return "npm_" + self.sensor_name + "_proxy_hosts"
+
+    @property
+    def native_value(self):
+        """Return the native value of the sensor."""
+        if self.sensor_name == "enabled":
+            return self.coordinator.api.num_redir_enabled
+        return self.coordinator.api.num_redir_disabled
 
     @property
     def icon(self):
