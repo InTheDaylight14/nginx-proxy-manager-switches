0255bf41e866b984b6b519d5ff1351d1e374409b
MillerKyle72 <millerkyle72@gmail.com>
2025-10-15 21:13:01 +0000

    add renew buttons

---
+++ custom_components/npm_switches/button.py
@@ -0,0 +1,44 @@
+"""Button platform for NPM Switches."""
+from datetime import datetime
+
+from homeassistant.components.button import ButtonEntity
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.util import slugify, dt
+
+from .const import DOMAIN
+from .entity import NpmSwitchesEntity
+from . import NpmSwitchesUpdateCoordinator
+
+
+async def async_setup_entry(hass, entry, async_add_entities):
+    """Setup sensor platform."""
+    coordinator = hass.data[DOMAIN][entry.entry_id]
+    api = hass.data[DOMAIN][entry.entry_id].api
+    certificates = await api.get_certificates()
+    entities = []
+
+    for cert in certificates.values():
+        entities.append(NpmSwitchesCertificateRenewButton(coordinator, entry, cert))
+
+    async_add_entities(entities, True)
+
+class NpmSwitchesCertificateRenewButton(NpmSwitchesEntity, ButtonEntity):
+    """NPM Switches Certificate Renew Button Class."""
+
+    def __init__(
+        self,
+        coordinator: NpmSwitchesUpdateCoordinator,
+        entry: ConfigEntry,
+        certificate: dict,
+    ) -> None:
+        """Initialize cert renew button."""
+        super().__init__(coordinator, entry)
+        self.cert_id = str(certificate["id"])
+        self.name = "Renew Certificate " + certificate["domain_names"][0]
+        self.entity_id = "button."+slugify(f"{entry.title}")+" Renew Cert "+str(self.cert_id)
+        self._attr_unique_id = f"{entry.entry_id} {" Renew Cert "} {self.cert_id}"
+        self._attr_icon = "mdi:refresh"
+
+    async def async_press(self) -> None:
+        """Handle the button press."""
+        await self.coordinator.api.renew_certificate(self.cert_id)
